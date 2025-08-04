ec71e9e20272e806a72a46d3d4e0dbb11c277e94
VicoAritonang <vicoaritonang.2611@gmail.com>
2025-08-04 17:44:57 +0700

    gasen

--- next.config.js
+++ next.config.js
@@ -2,47 +2,6 @@
 const nextConfig = {
   images: {
     domains: ['i.imgur.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-        ],
-      },
-    ]
-  },
-  async redirects() {
-    return [
-      {
-        source: '/home',
-        destination: '/',
-        permanent: true,
-      },
-    ]
   },
 }
 
